f37e62e97127bdf0330c7c4a034d4fc9922bb703
Wen Cong <S10194723@connect.np.edu.sg>
2020-11-05 09:06:35 +0800

    Initial Commit

---
+++ Practical3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practical3
+//
+//  Created by Yeo Wen Cong on 5/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
