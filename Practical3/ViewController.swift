dcbd83746247a254e9c38b7e1c668b14e18c875e
Wen Cong <S10194723@connect.np.edu.sg>
2020-11-05 10:24:50 +0800

    Completed Week 3 practical

--- Practical3/ViewController.swift
+++ Practical3/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var MyLabel: UILabel!
+    @IBOutlet weak var txtField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func OnClick(_ sender: AnyObject) {
+        print("I'm Clicked!")
+        MyLabel.text = "I'm Clicked!"
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        txtField.resignFirstResponder()
+        MyLabel.text = textField.text
+        return true
+    }
 
 }
 
